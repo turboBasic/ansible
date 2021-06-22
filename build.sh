b12a5449e38244445c2678fd38f9a3015d176017
turboBasic ⚡💀⚡ <off@boun.cr>
2021-06-22 11:33:02 +0300

    Add shellcheck directives to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 main() {
+    # shellcheck disable=SC2155
+
     local SCRIPT_DIR=$( dirname "$( realpath "${BASH_SOURCE[0]}" )" )
     echo "SCRIPT_DIR: ${SCRIPT_DIR}"
 
@@ -8,6 +10,7 @@ main() {
 }
 
 
+# shellcheck disable=SC2034
 if [ -n "$ZSH_EVAL_CONTEXT" ]; then
     echo "ERROR: This script should be executed by bash shell. Current shell is identified as 'zsh'"
     case $ZSH_EVAL_CONTEXT in
@@ -34,4 +37,3 @@ else # All other shells: examine $0 for known shell binary filenames
     esac
     exit 1
 fi
-
